ed92926c4d77b409e3c782f98eaac1197fbbc8b4
Diljeetsingh123 <110691680+Diljeetsingh123@users.noreply.github.com>
2022-08-20 23:51:50 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+var x=document.getElementById("login");
+    var y=document.getElementById("signup");
+    var z=document.getElementsByClassName("togglebtn");
+    
+    function signup(){
+        x.style.left="-400px";
+        y.style.left="20px";
+        
+    }
+    function login(){
+        x.style.left="20px";
+        y.style.left="450px";
+    }
+    
